796cd81cdb645809d1e7a87fc79cbbdf4f6aa628
Hossain Khan <hello@hossain.dev>
2025-10-03 23:26:22 -0400

    [BUMP] App version for next release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "ink.trmnl.android.buddy"
         minSdk = 28
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
 
         // Read key or other properties from local.properties
         val localProperties =
