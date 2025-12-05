7fbe308d6340c915fb12c8958047e9338a85c6b6
Hossain Khan <hello@hossain.dev>
2025-12-05 16:00:41 -0500

    chore: Prepare release 2.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
 
         // Google Play app versioning - keep in sync with release notes and changelog
         // See https://github.com/hossain-khan/trmnl-android-buddy/blob/main/keystore/README.md#release-keystore-production
-        versionCode = 23
-        versionName = "2.7.0"
+        versionCode = 24
+        versionName = "2.7.1"
 
         // Read key or other properties from local.properties
         val localProperties =
